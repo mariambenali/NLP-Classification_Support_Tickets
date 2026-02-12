84f65e00a1e18c10ad4041f3950e1b691e2b2a61
mariambenali <miriam.bena@gmail.com>
2026-02-12 16:50:26 +0100

    modify script github actions v8

--- monitoring/evidently_monitor.py
+++ monitoring/evidently_monitor.py
@@ -10,12 +10,14 @@ def run_evidently_monitoring():
     # load data
     BASE_DIR = "/Users/miriambenali/Desktop/Project-Simplon/NLP-Classification_Support_Tickets--with-MLOps"
     file_path = os.path.join(
-        BASE_DIR, 
+        BASE_DIR,
         "data/processed/tickets_clean.csv")
     df = pd.read_csv(file_path)
 
     # load model
-    model_path = os.path.join(BASE_DIR, "models/rfc_ticket_model.pkl")
+    model_path = os.path.join(
+        BASE_DIR,
+        "models/rfc_ticket_model.pkl")
     model = load(model_path)
 
     # model embeddings
@@ -30,11 +32,13 @@ def run_evidently_monitoring():
     # generate predictions
     # embedding & normalize
     reference_embeddings = model_embeddings.encode(
-        reference_data["clean_text"].tolist(), normalize_embeddings=True
+        reference_data["clean_text"].tolist(),
+        normalize_embeddings=True
     )
 
     current_embeddings = model_embeddings.encode(
-        current_data["clean_text"].tolist(), normalize_embeddings=True
+        current_data["clean_text"].tolist(),
+        normalize_embeddings=True
     )
 
     # prediction
@@ -46,41 +50,48 @@ def run_evidently_monitoring():
         categorical_columns=["queue", "priority", "language", "type", "prediction"],
         classification=[
             MulticlassClassification(
-                target="type", 
+                target="type",
                 prediction_labels="prediction")
-        ],
+        ]
     )
     reference_dataset = Dataset.from_pandas(
-        reference_data, data_definition=data_definition
+        reference_data,
+        data_definition=data_definition
     )
 
-    current_dataset = Dataset.from_pandas(current_data, data_definition=data_definition)
+    current_dataset = Dataset.from_pandas(
+        current_data,
+        data_definition=data_definition)
 
     # report data drift
     drift_result = Report(metrics=[DataDriftPreset()]).run(
-        reference_dataset, current_dataset
-    )
-    classification_result = Report(metrics=[ClassificationPreset()]).run(
-        reference_dataset, current_dataset
-    )
+        reference_dataset,
+        current_dataset
+        )
+    classification_result = Report(
+        metrics=[ClassificationPreset()]).run(
+            reference_dataset,
+            current_dataset
+        )
     # save the report
-    reports_path = os.path.join(BASE_DIR, "reports")
+    reports_path = os.path.join(
+        BASE_DIR,
+        "reports")
     os.makedirs(reports_path, exist_ok=True)
 
     # data drift
     drift_result.save_html(
         os.path.join(
-            reports_path, 
+            reports_path,
             "data_drift_report.html"))
 
     # Classification
     classification_result.save_html(
         os.path.join(
-            reports_path, 
+            reports_path,
             "classification_report.html")
     )
 
     print("Monitoring terminé")
-
 
 run_evidently_monitoring()
